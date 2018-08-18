510c52a7fc0566b4526ecadd9b5ae7f52c9aaac3
Lina Lockheart <angelinalockheart@gmail.com>
2018-08-18 18:23:07 -0400

    hid keys

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
+require('dotenv').config();
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   });
   
   connection.connect(function(err) {
